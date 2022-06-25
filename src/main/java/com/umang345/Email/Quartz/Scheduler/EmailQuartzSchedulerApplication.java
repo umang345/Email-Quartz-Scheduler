6546a24fe35bbabcc21cc156545feb4ca4e507d3
Umang <umangagarwal345@gmail.com>
2022-06-25 15:21:52 +0530

    Spring boot project setup

---
+++ src/main/java/com/umang345/Email/Quartz/Scheduler/EmailQuartzSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.umang345.Email.Quartz.Scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailQuartzSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailQuartzSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umang345/Email/Quartz/Scheduler/EmailQuartzSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umang345.Email.Quartz.Scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailQuartzSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
