aba2fbb6bc845f206ea46aa3546ca82f9b96f0b0
Umang <umangagarwal345@gmail.com>
2022-06-27 06:44:30 +0530

    Added lombok annotations

--- src/main/java/com/umang345/Email/Quartz/Scheduler/payload/EmailResponse.java
+++ src/main/java/com/umang345/Email/Quartz/Scheduler/payload/EmailResponse.java
@@ -1,5 +1,12 @@
 package com.umang345.Email.Quartz.Scheduler.payload;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
 public class EmailResponse
 {
     private boolean success;
